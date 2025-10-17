4c86d67f75871e420627e3b78770f314b57a4b5e
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-10-17 13:59:21 -0700

    Adding overflow-hidden and max-width on body

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased max-w-screen overflow-x-hidden  flex flex-col min-h-screen`}
       >
         {children}
         <Footer />
