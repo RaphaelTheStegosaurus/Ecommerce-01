8987951b5aa48d90302658905a1ac14410c65df3
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-10-17 13:44:21 -0700

    Adding a header on Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
-import Footer from "./components/footer";
-
 export default function Home() {
   return (
     <>
-      <header></header>
+      <header className="pl-4 min-h-screen flex flex-col">
+        <h1 className=" py-6 text-4xl flex flex-col font-extrabold">
+          Nueva Colección
+          <span className="">Primavera 2024</span>
+        </h1>
+        <p className="my-6 font-light text-2xl">
+          Descubre nuestra selección curada de productos elegantes con diseños
+          minimalistas y colores que inspiran.
+        </p>
+        <div className="homeButtones flex flex-row gap-2">
+          <a
+            className="text-white bg-pink-400 px-6 py-3 rounded-full text-xl"
+            href="#"
+          >
+            Explorar Ahora
+          </a>
+          <a className="px-6 py-3 rounded-full text-xl" href="#">
+            Ver Ofertas
+          </a>
+        </div>
+      </header>
       <main></main>
-      <Footer />
     </>
   );
 }
