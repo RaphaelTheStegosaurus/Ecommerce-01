bbaab936a2c905a8dfa2a3565e966db986ab93c9
RaphaelTheStegosaurus <r.a.s.m.13@hotmail.com>
2025-10-17 21:57:19 -0700

    Adding Styles with tailwind Adding responsive design

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -18,25 +18,42 @@ const Navbar = () => {
     );
   });
   return (
-    <nav>
-      <span> Shop</span>
-      <ul>{NavLinks}</ul>
-      <div className="searchbox">
-        <button>
+    <nav className="bg-orange-400 max-w-screen px-4 py-4 flex flex-row items-center justify-between sticky top-0">
+      <h2 className="text-2xl">Shop</h2>
+      <ul className="md:flex md:flex-row hidden gap-5 text-lg">{NavLinks}</ul>
+      <div className="hidden md:flex md:flex-row searchbox rounded-full shadow-sm border-2  border-orange-300 focus-within:border-white items-center">
+        <button className="w-5 h-5 text-white font-bold text-md ml-2">
           <CiSearch />
         </button>
-        <input type="search" name="searchbox" id="searchbox" />
+        <input
+          className="w-full py-1 px-3 text-white leading-tight focus:outline-none bg-transparent"
+          type="search"
+          name="searchbox"
+          id="searchbox"
+        />
       </div>
-      <a href="#">
+      <a
+        href="#"
+        className="hover:bg-white p-3 rounded-2xl transition-colors text-2xl hidden md:block"
+      >
         <CiUser />
       </a>
-      <a href="#">
+      <a
+        className="hover:bg-white p-3 rounded-2xl transition-colors text-2xl hidden md:block"
+        href="#"
+      >
         <CiHeart />
       </a>
-      <a href="#">
+      <a
+        className="hover:bg-white p-3 rounded-2xl transition-colors text-2xl hidden md:block"
+        href="#"
+      >
         <CiShoppingCart />
       </a>
-      <a href="#">
+      <a
+        className="hover:bg-white p-3 rounded-2xl transition-colors text-2xl md:hidden"
+        href="#"
+      >
         <CiMenuBurger />
         <IoMdClose />
       </a>
